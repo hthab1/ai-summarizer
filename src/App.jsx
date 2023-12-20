5070d4f194b10b2963baab41374b4fcae4ccf562
Habteweld Yimer <ht@Habtewelds-MacBook-Pro.local>
2023-12-20 13:29:52 +0300

    app done

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,11 @@ function App() {
   return (
     <main>
       <div className="main">
-        <div className="gradient">
-          <div className="app">
-            <Hero />
-            <Demo />
-          </div>
-        </div>
+        <div className="gradient"></div>
+      </div>
+      <div className="app">
+        <Hero />
+        <Demo />
       </div>
     </main>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,16 +4,26 @@ import { logo } from "../assets";
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
-      <nav className="flezx justify-between items-center w-full mb-10 pt-3">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
         <img src={logo} alt="sumz-logo" className="w-28 object-contain" />
         <button
           type="button"
           className="black_btn"
-          onClick={() => window.open("")}
+          onClick={() =>
+            window.open("https://github.com/hthab1/ai-summarizer.git")
+          }
         >
           Github
         </button>
       </nav>
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPR-4</span>
+      </h1>
+      <h2 className="desc">
+        Simplify your teading with Summize, an open-source article lengthy
+        articles into clear and concise summaries
+      </h2>
     </header>
   );
 };
